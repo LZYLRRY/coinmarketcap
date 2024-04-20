db3eab4098b328af97aca879d048c0bedb7f3206
LZYLRRY <162039530+LZYLRRY@users.noreply.github.com>
2024-04-20 21:14:30 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+  <RouterView />
+</template>
+
+<style scoped>
+  nav
+  {
+    background-color: blue;
+    text-align: center;
+    padding: 20px;
+  }
+  
+  a
+  {
+    color: white;
+    margin: 10px;
+  }
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<script>
+/* Example in Node.js */
+
+const axios = require('axios'); // hier krijg ik dus een error code
+// import axios from 'axios'; Hier krijg ik geen error code maar allerlei andere errors
+
+let response = null;
+new Promise(async (resolve, reject) => {
+  try {
+    response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
+      headers: {
+        'X-CMC_PRO_API_KEY': '16749118-b7c5-420c-9fb8-15ed1253abe0',
+        // api key hierboven is dus mijn eigen API key
+      },
+    });
+  } catch(ex) {
+    response = null;
+    // error
+    console.log(ex);
+    reject(ex);
+  }
+  if (response) {
+    // success
+    const json = response.data;
+    console.log(json);
+    resolve(json);
+  }
+});
+
+</script>
+
+<template>
+
+</template>
